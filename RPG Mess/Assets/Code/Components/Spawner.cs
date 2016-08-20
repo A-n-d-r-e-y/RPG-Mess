d72a4f4cb3030fd255b7edc03fa3e787a7fdee50
Andrey <andrey@morozov.lv>
2016-08-20 14:38:30 +0300

    enemy spawner implemented in #7

---
+++ RPG Mess/Assets/Code/Components/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Spawner : MonoBehaviour
+{
+    private DateTime time_save = DateTime.Now;
+
+    [SerializeField]
+    GameObject prefabToSpawn;
+    [SerializeField]
+    private double spawnRate = 5;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        double delay_ms = 1000 / spawnRate;
+
+        if (time_save.AddMilliseconds(delay_ms) <= DateTime.Now)
+        {
+            UnityEngine.Object.Instantiate(
+                prefabToSpawn,
+                transform.position,
+                Quaternion.identity);
+
+            time_save = DateTime.Now;
+        }
+    }
+}
